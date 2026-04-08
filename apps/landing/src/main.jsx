47223f89323616a9907b18706a28f2d064cc2939
Dehan <dehan@vodafonemail.de>
2026-04-08 14:41:04 +0800

    feat(landing page): new

---
+++ apps/landing/src/main.jsx
@@ -0,0 +1,30 @@
+// 本地字体，替代 Google Fonts CDN
+import "@fontsource/dm-sans/300.css";
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/500.css";
+import "@fontsource/dm-sans/600.css";
+import "@fontsource/dm-sans/700.css";
+import "@fontsource/source-serif-4/300.css";
+import "@fontsource/source-serif-4/400.css";
+import "@fontsource/source-serif-4/500.css";
+import "@fontsource/source-serif-4/600.css";
+import "@fontsource/source-serif-4/700.css";
+import "@fontsource/noto-serif-sc/400.css";
+import "@fontsource/noto-serif-sc/500.css";
+import "@fontsource/noto-serif-sc/600.css";
+import "@fontsource/noto-serif-sc/700.css";
+import "@fontsource/noto-sans-sc/300.css";
+import "@fontsource/noto-sans-sc/400.css";
+import "@fontsource/noto-sans-sc/500.css";
+import "@fontsource/noto-sans-sc/600.css";
+import "@fontsource/noto-sans-sc/700.css";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import LandingPage from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <LandingPage />
+  </StrictMode>
+);
