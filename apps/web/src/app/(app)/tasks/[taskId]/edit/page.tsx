d697292dac12aa9b3deeb8a7ff0cf8ee363160e3
Dehan <dehan@vodafonemail.de>
2026-05-07 17:29:37 +0800

    feat: add late submission in task editor

--- apps/web/src/app/(app)/tasks/[taskId]/edit/page.tsx
+++ apps/web/src/app/(app)/tasks/[taskId]/edit/page.tsx
@@ -111,6 +111,7 @@ export default function EditTaskPage() {
       initialAttachments={task.attachments}
       isAlreadyPublished={task.isPublished}
       initialDueAt={task.dueAt ?? undefined}
+      initialAllowLateSubmission={task.allowLateSubmission}
     />
   );
 }
